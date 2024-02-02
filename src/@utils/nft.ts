1faadd2f7a40d509ec34904a61b649e92f2e4164
Moritz Kirstein <mkirstein@protonmail.com>
2024-02-02 16:29:24 +0100

    refactor: update nft description

--- src/@utils/nft.ts
+++ src/@utils/nft.ts
@@ -186,7 +186,7 @@ export async function setNFTMetadataAndTokenURI(
         : {
             name: (asset as AssetExtended).nft.name,
             symbol: (asset as AssetExtended).nft.symbol,
-            description: `${nftMetadataTemplate.description}\n\nView on Pontus-X: ${externalUrl}`,
+            description: `${nftMetadataTemplate.description}\n\nView on Ocean Enterprise: ${externalUrl}`,
             external_url: externalUrl
           }
     )
