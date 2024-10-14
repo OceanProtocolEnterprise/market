77be4fe68a926c3b911ccdb6b1ff29e238c757c4
Daniel Simon <d.simon@neusta.de>
2024-10-14 12:05:39 +0200

    Cleaned up component

--- src/components/Publish/CustomDDO/index.tsx
+++ src/components/Publish/CustomDDO/index.tsx
@@ -1,25 +1,10 @@
-import { BoxSelectionOption } from '@shared/FormInput/InputElement/BoxSelection'
 import Input from '@shared/FormInput'
-import { Field, useField, useFormikContext } from 'formik'
-import { ReactElement, useEffect } from 'react'
+import { Field } from 'formik'
+import { ReactElement } from 'react'
 import content from '../../../../content/publish/form.json'
-import consumerParametersContent from '../../../../content/publish/consumerParameters.json'
-import { FormPublishData } from '../_types'
-import IconDataset from '@images/dataset.svg'
-import IconAlgorithm from '@images/algorithm.svg'
-import styles from './index.module.css'
-import { algorithmContainerPresets } from '../_constants'
-import { useMarketMetadata } from '@context/MarketMetadata'
 import { getFieldContent } from '@utils/form'
 
 export default function CustomDDOFields(): ReactElement {
-  const { siteContent } = useMarketMetadata()
-
-  // connect with Form state, use for conditional field rendering
-  const { values, setFieldValue } = useFormikContext<FormPublishData>()
-
-  const [field, meta] = useField('metadata.dockerImageCustomChecksum')
-
   return (
     <>
       <Field
