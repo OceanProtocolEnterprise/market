dfbd51a73199020d207f514b91d7cb753cfada40
ahmedraza118 <ahmedrazachrtx@gmail.com>
2026-06-12 19:20:07 +0300

    removed console logs

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -113,7 +113,6 @@ export default async function handler(
       issuer: metadata.issuer,
       audience: clientId
     })
-    console.log('Payload:  ', payload)
     if (payload.nonce !== expectedNonce) return failRedirect(res)
 
     // Validate required claims — throws if any are missing or empty
